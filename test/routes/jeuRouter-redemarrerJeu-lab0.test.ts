cbac4940dd3aeb5217650043da69dda6b96f08ac
Merisiel0 <merisiel01@gmail.com>
2025-09-09 17:31:02 +0000

    step 3 done

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,32 @@
-// Vous devez insérer les nouveaux tests ici
+import supertest from 'supertest';
 import { assert } from 'console';
 import 'jest-extended';
+import app from '../../src/app';
+import { jeuRoutes } from "../../src/routes/jeuRouter";
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+
+const testNom1 = 'Jean-Marc';
+const testNom2 = 'Pierre';
+
+beforeAll(async () => {
+    await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom1 });
+    await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom2 });
+});
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  it("devrait répondre avec un appel réussi en json", async () => {
+    const response = await request.get("/api/v1/jeu/redemarrerJeu");
+    const resultat = JSON.parse(response.body.resultat);
+    expect(response.type).toBe("application/json");
+    expect(resultat.status).toBe(200);
+  });
+
+  it("devrait ne plus y avoir de joueur", async () =>{
+    const response = await request.post('/api/v1/jeu/redemarrerJeu').send({ nom: testNom1 });
+    expect(response.statusCode).toBe(200);
+    const joueursJSON = jeuRoutes.controleurJeu.joueurs;
+    const joueursArray = JSON.parse(joueursJSON);
+    expect(joueursArray.length).toBe(0);
   });
 });
